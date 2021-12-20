30d0fb7b96b9ccd01b43bb7d8eb204e99c257aca
mac <harrykurniawan@email.unikom.ac.id>
2021-12-20 16:06:09 +0700

    First Commit

---
+++ Assets/Script/LoadingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static int TargetSceneIndex = 1;
+    public Slider LoadingBar;
+    AsyncOperation async;
+
+    IEnumerator StartLoadingProgress(int index){
+        async = SceneManager.LoadSceneAsync (index);
+        async.allowSceneActivation = false;
+        while (async.isDone == false){
+            LoadingBar.value = async.progress;
+            if (async.progress >= 0.9f){
+                LoadingBar.value = 1;
+                async.allowSceneActivation = true;
+            }
+            yield return new WaitForSeconds (3);
+            yield return null;
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(StartLoadingProgress(TargetSceneIndex));       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsPause = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject pauseMenuUI2;
+    public GameObject pauseMenuUI3;
+    public GameObject pauseMenuUI4;
+
+    public AudioMixer audioMixer;
+
+    
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++){
+
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
+                currentResolutionIndex = i;
+
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+
+    public void SetVolume (float volume){
+       audioMixer.SetFloat("volume", volume);
+   }
+
+   public void SetQuality(int qualityIndex){
+       QualitySettings.SetQualityLevel(qualityIndex);
+       
+   }
+
+   public void SetFullscreen (bool isFullscreen){
+       Screen.fullScreen = isFullscreen;
+   }
+
+   public void SetResolution (int resolutionIndex){
+       Resolution resolution = resolutions[resolutionIndex];
+       Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+   }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPause)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() 
+    {
+        pauseMenuUI.SetActive(false);
+        pauseMenuUI2.SetActive(false);
+        pauseMenuUI3.SetActive(false);
+        pauseMenuUI4.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+ 
+    void Pause() 
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
